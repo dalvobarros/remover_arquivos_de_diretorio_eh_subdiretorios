5a86c559094e715ddb8f161f0cfab5f1ea3c4f1e
dalvobarros <39811034+dalvobarros@users.noreply.github.com>
2020-05-13 09:26:05 -0300

    Update RemoverArquivosDeDiretorio.py

--- RemoverArquivosDeDiretorio.py
+++ RemoverArquivosDeDiretorio.py
@@ -1,39 +1,53 @@
 import os
 from abc import ABC, abstractmethod
 
-class RemoverFiles:
-    
+class RemoverFile:
+
     @abstractmethod
     def removerArqDiretorioEhSubDiretorio(diretorio_remocao, remocao):
 
-        lista_remocao = ['implements', 'Track3D', 'Materials', 'CoverageHistory', 'LineFeature', 'Swaths', 'EventHistory', 'newField', 'origin', 'EventSummary']
-       
-        lista_remocao.append(remocao)        
+        lista_remocao = ['implements', 'Track3D', 'Materials', 'CoverageHistory', 'Swaths', 'EventHistory', 'newField', 'origin', 'EventSummary']
+
+        lista_remocao.extend(remocao)
 
         from datetime import datetime
         data_e_hora_atuais = datetime.now()
         formato_data_e_hora = '%d/%m/%Y %H:%M:%S'
         time_inicio = data_e_hora_atuais.strftime(formato_data_e_hora)
         print("Remoção de arquivos iniciada..... às: {0}".format(time_inicio))
-        
-        diretorio = os.listdir(diretorio_remocao)
-        registros_excluidos = 0
-        for pasta in diretorio:
-            diretorio_atual = diretorio_remocao + "\\" + pasta
-            for root, dirs, files in os.walk(diretorio_atual):
-                for file in files:
-                    for list in lista_remocao:
-                        if file.startswith(str(list)) or file.endswith('.pos'):
-                            try:
-                                print("Arquivo removido....: {0}".format(file))                                
-                                registros_excluidos = registros_excluidos + 1
-                                os.remove(os.path.join(root, file))
-                            except:
-                                print("Ops.... Erro ao remover o arquivo:".format(file))
-                        else:                            
-                            pass
+
+        try:
+            registros_excluidos = 0
+
+            if os.path.exists(diretorio_remocao):
+                diretorio = os.listdir(diretorio_remocao)
+                for pasta in diretorio:
+                    diretorio_atual = diretorio_remocao + "\\" + pasta
+                    for root, dirs, files in os.walk(diretorio_atual):
+                        for file in files:
+                            for list in lista_remocao:
+                                if file.startswith(str(list)) or file.endswith('.pos'):
+                                    try:
+                                        print("Arquivo removido....: {0}".format(file))
+                                        registros_excluidos = registros_excluidos + 1
+                                        os.remove(os.path.join(root, file))
+                                    except:
+                                        print("Ops.... Erro ao remover o arquivo:".format(file))
+                                else:
+                                    pass
+            else:
+                print("Diretório não existente: {0}, Por gentileza informe outro".format(diretorio_remocao))
+
+        except OSError:
+            print("Diretório não existente: {0}, Por gentileza informe outro".format(diretorio_remocao))
+            pass
+
         data_e_hora_atuais = datetime.now()
         time_termino = data_e_hora_atuais.strftime(formato_data_e_hora)
         time_total = datetime.strptime(time_termino, formato_data_e_hora) - datetime.strptime(time_inicio, formato_data_e_hora)
         print("Processo iniciado às: {0}, finalizado às: {1}".format(time_inicio, time_termino))
-        print("Total de arquivos excluídos: {0}, Tempo gasto: {1}".format(registros_excluidos, time_total)) 
+        print("Total de arquivos excluídos: {0}, Tempo gasto: {1}".format(registros_excluidos, time_total))
+
+##incluir = ['dalvo', 'myeverton']
+##
+##RemoverFile.removerArqDiretorioEhSubDiretorio("C:\\temp\\teste\\Nova pasta", incluir)
